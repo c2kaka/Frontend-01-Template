3004b584f35eb8f2290df4b182dbc7d19444f4ac
c2kaka <c2kaka@126.com>
2020-05-12 21:15:17 +0800

    toybrowser

--- week05/Client.js
+++ week05/Client.js
@@ -5,7 +5,7 @@ class Request {
 		this.method = options.method || 'GET';
 		this.host = options.host;
 		this.port = options.port || 80;
-		this.path = options.path;
+		this.path = options.path || '/';
 		this.body = options.body || {};
 		this.headers = options.headers || {};
 
@@ -16,7 +16,7 @@ class Request {
 		if (this.headers['Content-Type'] === 'application/json') {
 			this.bodyText = JSON.stringify(this.body);
 		} else if (
-			his.headers['Content-Type'] === 'application/x-www-form-urlencoded'
+			this.headers['Content-Type'] === 'application/x-www-form-urlencoded'
 		) {
 			this.bodyText = Object.keys(this.body)
 				.map((key) => `${key}=${encodeURIComponent(this.body[key])}`)
@@ -35,26 +35,51 @@ ${Object.keys(this.headers)
 ${this.bodyText}`;
 	}
 
-	send() {}
+	send(connection) {
+		return new Promise((resolve, reject) => {
+			if (connection) {
+				connection.write(this.toString());
+			} else {
+				connection = net.createConnection(
+					{
+						host: this.host,
+						port: this.port,
+					},
+					() => {
+						connection.write(this.toString());
+					}
+				);
+			}
+
+			connection.on('data', (data) => {
+				resolve(data.toString());
+				connection.end();
+			});
+
+			connection.on('error', (err) => {
+				reject(err);
+				connection.end();
+			});
+		});
+	}
 }
 
 class Response {}
 
-const client = net.createConnection({ host: '127.0.0.1', port: 8088 }, () => {
-	console.log('connected to server');
-	client.write(`
-POST / HTTP/1.1\r
-Content-Type: application/x-www-form-urlencoded\r
-Content-Length: 11\r
-\r
-name=winter`);
-});
+void (async function () {
+	let request = new Request({
+		method: 'POST',
+		host: '127.0.0.1',
+		port: 8088,
+		path: '/',
+		headers: {
+			'X-FOO2': 'kaka',
+		},
+		body: {
+			name: 'kaka',
+		},
+	});
 
-client.on('data', (data) => {
-	console.log(data.toString());
-	client.end();
-});
-
-client.on('end', () => {
-	console.log('disconnected from server');
-});
+	const response = await request.send();
+	console.log(response);
+})();
