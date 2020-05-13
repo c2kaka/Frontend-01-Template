9625a6e8d5de3febf023da5c467cee46b2bff337
c2kaka <c2kaka@126.com>
2020-05-13 21:38:53 +0800

    50%

--- week05/Client.js
+++ week05/Client.js
@@ -1,85 +1,166 @@
 const net = require('net');
 
 class Request {
-	constructor(options) {
-		this.method = options.method || 'GET';
-		this.host = options.host;
-		this.port = options.port || 80;
-		this.path = options.path || '/';
-		this.body = options.body || {};
-		this.headers = options.headers || {};
+  constructor(options) {
+    this.method = options.method || 'GET';
+    this.host = options.host;
+    this.port = options.port || 80;
+    this.path = options.path || '/';
+    this.body = options.body || {};
+    this.headers = options.headers || {};
 
-		if (!this.headers['Content-Type']) {
-			this.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-		}
+    if (!this.headers['Content-Type']) {
+      this.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+    }
 
-		if (this.headers['Content-Type'] === 'application/json') {
-			this.bodyText = JSON.stringify(this.body);
-		} else if (
-			this.headers['Content-Type'] === 'application/x-www-form-urlencoded'
-		) {
-			this.bodyText = Object.keys(this.body)
-				.map((key) => `${key}=${encodeURIComponent(this.body[key])}`)
-				.join('&');
-		}
+    if (this.headers['Content-Type'] === 'application/json') {
+      this.bodyText = JSON.stringify(this.body);
+    } else if (
+      this.headers['Content-Type'] === 'application/x-www-form-urlencoded'
+    ) {
+      this.bodyText = Object.keys(this.body)
+        .map((key) => `${key}=${encodeURIComponent(this.body[key])}`)
+        .join('&');
+    }
 
-		this.headers['Content-Length'] = this.bodyText.length;
-	}
+    this.headers['Content-Length'] = this.bodyText.length;
+  }
 
-	toString() {
-		return `${this.method} ${this.path} HTTP/1.1\r
+  toString() {
+    return `${this.method} ${this.path} HTTP/1.1\r
 ${Object.keys(this.headers)
-	.map((key) => `${key}: ${this.headers[key]}`)
-	.join('\r\n')}
+  .map((key) => `${key}: ${this.headers[key]}`)
+  .join('\r\n')}
 \r
 ${this.bodyText}`;
-	}
+  }
 
-	send(connection) {
-		return new Promise((resolve, reject) => {
-			if (connection) {
-				connection.write(this.toString());
-			} else {
-				connection = net.createConnection(
-					{
-						host: this.host,
-						port: this.port,
-					},
-					() => {
-						connection.write(this.toString());
-					}
-				);
-			}
+  send(connection) {
+    return new Promise((resolve, reject) => {
+      const parser = new ResponseParser();
+      if (connection) {
+        connection.write(this.toString());
+      } else {
+        connection = net.createConnection(
+          {
+            host: this.host,
+            port: this.port,
+          },
+          () => {
+            connection.write(this.toString());
+          }
+        );
+      }
 
-			connection.on('data', (data) => {
-				resolve(data.toString());
-				connection.end();
-			});
+      connection.on('data', (data) => {
+        parser.receive(data.toString());
+        console.log(parser.statusLine);
+        // resolve(data.toString());
+        connection.end();
+      });
 
-			connection.on('error', (err) => {
-				reject(err);
-				connection.end();
-			});
-		});
-	}
+      connection.on('error', (err) => {
+        reject(err);
+        connection.end();
+      });
+    });
+  }
 }
 
 class Response {}
 
-void (async function () {
-	let request = new Request({
-		method: 'POST',
-		host: '127.0.0.1',
-		port: 8088,
-		path: '/',
-		headers: {
-			'X-FOO2': 'kaka',
-		},
-		body: {
-			name: 'kaka',
-		},
-	});
+class ResponseParser {
+  constructor() {
+    this.WAITING_STATUS_LINE = 0;
+    this.WAITING_STATUS_LINE_END = 1;
+    this.WAITING_HEADER_NAME = 2;
+    this.WAITING_HEADER_SPACE = 3;
+    this.WAITING_HEADER_VALUE = 4;
+    this.WAITING_HEADER_LINE_END = 5;
+    this.WAITING_HEADER_BLOCK_END = 6;
+    this.WAITING_BODY = 7;
 
-	const response = await request.send();
-	console.log(response);
+    this.current = this.WAITING_STATUS_LINE;
+    this.statusLine = '';
+    this.headers = {};
+    this.headerName = '';
+    this.headerValue = '';
+  }
+
+  receive(string) {
+    for (let i = 0; i < string.length; i++) {
+      this.receiveCharacter(string.charAt(i));
+    }
+  }
+
+  receiveCharacter(char) {
+    if (this.current === this.WAITING_STATUS_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITING_STATUS_LINE_END;
+      } else {
+        this.statusLine += char;
+      }
+    } else if (this.current === this.WAITING_STATUS_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITING_HEADER_NAME) {
+      if (char === ':') {
+        this.current = this.WAITING_HEADER_SPACE;
+      } else if (char === '\r') {
+        this.current = this.WAITING_BODY;
+      } else {
+        this.headerName += char;
+      }
+    } else if (this.current === this.WAITING_HEADER_SPACE) {
+      if (char === ' ') {
+        this.current = this.WAITING_HEADER_VALUE;
+      }
+    } else if (this.current === this.WAITING_HEADER_VALUE) {
+      if (char === '\r') {
+        this.current = this.WAITING_HEADER_LINE_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerValue = '';
+        this.headerName = '';
+      } else {
+        this.headerValue += char;
+      }
+    } else if (this.current === this.WAITING_HEADER_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    }
+  }
+}
+
+class TrunkedParser {
+  constructor() {}
+
+  receive(string) {
+    for (let i = 0; i < string.length; i++) {
+      this.receiveCharacter(string.charAt(i));
+    }
+  }
+
+  receiveCharacter(char) {
+    // if (this.current)
+  }
+}
+
+void (async function () {
+  let request = new Request({
+    method: 'POST',
+    host: '127.0.0.1',
+    port: 8088,
+    path: '/',
+    headers: {
+      'X-FOO2': 'kaka',
+    },
+    body: {
+      name: 'kaka',
+    },
+  });
+
+  const response = await request.send();
+  console.log(response);
 })();
