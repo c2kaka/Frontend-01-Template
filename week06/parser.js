8f385355ac22d0c18ae90f1747537a51663ed8a2
c2kaka <c2kaka@126.com>
2020-05-20 18:02:04 +0800

    add css parser

--- week06/parser.js
+++ week06/parser.js
@@ -1,3 +1,5 @@
+const css = require('css');
+
 const EOF = Symbol('EOF');
 let currentToken;
 let currentAttribute;
@@ -6,6 +8,132 @@ let stack = [{
     type: 'document',
     children: []
 }];
+
+let rules = [];
+
+// 添加css rules
+function addCssRules(text) {
+    var ast = css.parse(text);
+    // console.log('ast: ', JSON.stringify(ast, null, '    '));
+    rules.push(...ast.stylesheet.rules);
+}
+
+// compute css
+function computeCss(element) {
+    // console.log(rules);
+    // console.log('computed css for Element', element);
+    // stack存放了所有的父元素
+    var elements = stack.slice().reverse();
+    if (!element.computedStyle) {
+        element.computedStyle = [];
+
+        for (const rule of rules) {
+            var selectorParts = rule.selectors[0].split(" ").reverse();
+            if (!match(element, selectorParts[0])) {
+                // current element donot match seletor
+                continue;
+            }
+            let matched = false;
+            var j = 1;
+            for (var i = 0; i < elements.length; i++) {
+                if (match(elements[i], selectorParts[j])) {
+                    j++
+                }
+                if (j >= selectorParts.length) {
+                    matched = true;
+                }
+                if (matched) {
+                    // 如果匹配到，我们要加入
+                    // console.log(elements);
+                    var sp = specificity(rule.selectors[0])
+                    var computedStyle = element.computedStyle;
+
+                    for (const declaration of rule.declarations) {
+                        if (!computedStyle[declaration.property]) {
+                            computedStyle[declaration.property] = {}
+                        }
+                        // 后面的属性可以覆盖已有的属性，未处理权重的问题
+                        // computedStyle[declaration.property].value = declaration.value;
+                        // 处理选择器权重，未处理内联和！important
+                        if (!computedStyle[declaration.property].specificity) {
+                            computedStyle[declaration.property].value = declaration.value;
+                            computedStyle[declaration.property].specificity = sp;
+                        } else if (compare(computedStyle[declaration.property].specificity, sp) <= 0) {
+                            computedStyle[declaration.property].value = declaration.value;
+                            computedStyle[declaration.property].specificity = sp;
+                        }
+                    }
+                    // console.log(element.tagName ,element.computedStyle)
+                }
+            }
+        }
+    }
+
+    function match(element, seletor) {
+        /**
+         main>div.a#id[attr=value]
+         main>
+         div
+         .a
+         #id
+         [attr=value]
+         *
+         */
+        if (!seletor || !element.attributes) {
+            return false;
+        }
+        let temp = seletor.charAt(0)
+        // 匹配id选择器
+        if (temp == '#') {
+            var attr = element.attributes.filter(attr => attr.name === 'id')[0];
+            if (attr && attr.value === seletor.replace('#', '')) {
+                return true;
+            }
+        } else if (temp === '.') {
+            // class选择器
+            var attr = element.attributes.filter(attr => attr.name === 'class')[0];
+
+            if (attr && attr.value === seletor.replace('.', '')) {
+                return true;
+            }
+        } else {
+            // 简单选择器
+            if (element.tagName === seletor) {
+                return true;
+            }
+        }
+        return false
+    }
+}
+
+// 确定权重
+function specificity(seletor) {
+    var p = [0, 0, 0, 0];
+    var selectorParts = seletor.split(" ");
+    for (const part of selectorParts) {
+        if (part.charAt(0) === '#') {
+            p[1] += 1;
+        } else if (part.charAt(0) === '.') {
+            p[2] += 1;
+        } else {
+            p[3] += 1;
+        }
+    }
+    return p;
+}
+
+function compare(sp1, sp2) {
+    if (sp1[0] - sp2[0]) {
+        return sp1[0] - sp2[0];
+    }
+    if (sp1[1] - sp2[1]) {
+        return sp1[1] - sp2[1];
+    }
+    if (sp1[2] - sp2[2]) {
+        return sp1[2] - sp2[2];
+    }
+    return sp1[3] - sp2[3]
+}
 
 function emit(token) {
     // console.log(token);
@@ -40,6 +168,9 @@ function emit(token) {
         if (top.tagName != token.tagName) {
             throw new Error(`Tag start end doesn't match!`);
         } else {
+            if (top.tagName == 'style') {
+                addCssRules(top.children[0].content);
+            }
             stack.pop();
         }
         currentTextNode = null;
