3ceaa01b8cc795cce1b1c7de76270e71d6be4e28
c2kaka <c2kaka@126.com>
2020-07-11 11:07:21 +0800

    比较完整D1JSX实现

--- week14/component/main.js
+++ week14/component/main.js
@@ -1,52 +1,124 @@
 function createElement(Cls, attributes, ...children) {
-  let element = new Cls();
-  
-  for (const name in attributes) {
-    // element[name] = attributes[name];
-    element.setAttribute(name, attributes[name]);
-  }
+	let o;
 
-  for (const child of children) {
-    element.appendChild(child);
-  }
+	if (typeof Cls === 'string') {
+		o = new Wrapper(Cls);
+	} else {
+		o = new Cls({
+			timer: {},
+		});
+	}
 
-  return element;
+	for (let name in attributes) {
+		o.setAttribute(name, attributes[name]);
+	}
+
+	//console.log(children);
+	console.log(o);
+	for (let child of children) {
+		if (typeof child === 'string') child = new Text(child);
+
+		o.appendChild(child);
+	}
+
+	return o;
 }
 
-class Div {
-  constructor(config) {
-    this.children = [];
-    this.root = document.createElement('div');
-  }
-
-  set class(v) { // property
-    console.log("Parent::class", v);
-  }
-
-  setAttribute(name, value) { //attribute
-    this.root.setAttribute(name, value);
-  }
-
-  appendChild(child) { //child
-    this.children.push(child);
-  }
-
-  mountTo(parent) {
-    parent.appendChild(this.root);
-
-    for (const child of this.children) {
-      child.mountTo(this.root);
-    }
-  }
+class Text {
+	constructor(text) {
+		this.children = [];
+		this.root = document.createTextNode(text);
+	}
+	mountTo(parent) {
+		parent.appendChild(this.root);
+	}
 }
+
+class Wrapper {
+	constructor(type) {
+		this.children = [];
+		this.root = document.createElement(type);
+	}
+
+	setAttribute(name, value) {
+		//attribute
+		this.root.setAttribute(name, value);
+	}
+
+	appendChild(child) {
+		this.children.push(child);
+	}
+
+	mountTo(parent) {
+		parent.appendChild(this.root);
+
+		for (let child of this.children) {
+			child.mountTo(this.root);
+		}
+	}
+}
+
+class MyComponent {
+	constructor(config) {
+		this.children = [];
+	}
+
+	setAttribute(name, value) {
+		//attribute
+		this.root.setAttribute(name, value);
+	}
+
+	appendChild(child) {
+		this.children.push(child);
+	}
+
+	render() {
+		return (
+			<article>
+				<header>I'm a header</header>
+				{this.slot}
+				<footer>I'm a footer</footer>
+			</article>
+		);
+	}
+
+	mountTo(parent) {
+		this.slot = <div></div>;
+		for (let child of this.children) {
+			debugger;
+			this.slot.appendChild(child);
+		}
+		this.render().mountTo(parent);
+	}
+}
+
+/*let component = <div id="a" cls="b" style="width:100px;height:100px;background-color:lightgreen">
+        <div></div>
+        <p></p>
+        <div></div>
+        <div></div>
+    </div>*/
 
 let component = (
-	<Div id ='a' class='b' style='height:100px;width:100px;background:blue'>
-		<Div />
-		<Div />
-		<Div />
-	</Div>
+	<MyComponent>
+		<div>text text text</div>
+	</MyComponent>
 );
-;
 
 component.mountTo(document.body);
+/*
+var component = createElement(
+    Parent, 
+    {
+        id: "a",
+        "class": "b"
+    }, 
+    createElement(Child, null), 
+    createElement(Child, null), 
+    createElement(Child, null)
+);
+*/
+
+console.log(component);
+
+//componet.setAttribute("id", "a");
