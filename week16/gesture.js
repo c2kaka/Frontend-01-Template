1391606a2f78a04b2cd9af1eafad159bad0ef0ce
Kaka <c2kaka@126.com>
2020-07-27 16:31:32 +0800

    完成基本事件监听

--- week16/gesture.js
+++ week16/gesture.js
@@ -1,15 +1,61 @@
 let element = document.getElementById('container');
 
-element.addEventListener('mousedown', () => {
-  let move = e => {
-    console.log(e.clientX, event.clientY);
+element.addEventListener('mousedown', e => {
+  start(e);
+  let mouseMove = e => {
+    move(e);
   };
 
-  let end = e => {
-    document.removeEventListener('mousemove', move);
-    document.removeEventListener('mouseup', end);
+  let mouseEnd = e => {
+    end(e);
+    document.removeEventListener('mousemove', mouseMove);
+    document.removeEventListener('mouseup', mouseEnd);
   }
 
-  document.addEventListener('mousemove', move);
-  document.addEventListener('mouseup', end);
+  document.addEventListener('mousemove', mouseMove);
+  document.addEventListener('mouseup', mouseEnd);
 });
+
+element.addEventListener('touchstart', (e) => {
+  for(let touch of e.changedTouches) {
+    start(touch);
+  }
+})
+
+element.addEventListener('touchmove', (e) => {
+  for(let touch of e.changedTouches) {
+    move(touch);
+  }
+})
+
+element.addEventListener('touchend', (e) => {
+  for(let touch of e.changedTouches) {
+    end(touch);
+  }
+})
+
+element.addEventListener('touchcancel', (e) => {
+  for(let touch of e.changedTouches) {
+    cancel(touch);
+  }
+})
+
+let start = (point) => {
+  console.log('start');
+  console.log(point.clientX, point.clientY);
+}
+
+let move = (point) => {
+  console.log('move');
+  console.log(point.clientX, point.clientY);
+}
+
+let end = (point) => {
+  console.log('end');
+  console.log(point.clientX, point.clientY);
+}
+
+let cancel = (point) => {
+  console.log('cancel');
+  console.log(point.clientX, point.clientY);
+}
