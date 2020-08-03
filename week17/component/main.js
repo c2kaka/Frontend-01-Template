88a74076caf10da9b2e9e9d55a916d02c5e84ae4
Kaka <c2kaka@126.com>
2020-08-03 17:57:28 +0800

    完成ListView组件

--- week17/component/main.js
+++ week17/component/main.js
@@ -1,5 +1,6 @@
 import {createElement, Text, Wrapper} from './createElement';
 import {TabPanel} from  './TabPanel'
+import {ListView} from './ListView'
 
 // let component = <Carousel data={[
 // 		'https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg',
@@ -8,14 +9,19 @@ import {TabPanel} from  './TabPanel'
 // 	'https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg',
 // ]} />
 
-let component = <TabPanel title='TabPanel Component'>
-	<span title="title1">This is content1</span>
-	<span title="title2">This is content2</span>
-	<span title="title3">This is content3</span>
-	<span title="title4">This is content4</span>
-	<span title="title5">This is content5</span>
-</TabPanel>
+let data = [
+	{title: '蓝猫', url: 'https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg'},
+	{title: '橘猫加白', url: 'https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg'},
+	{title: '狸花猫', url: 'https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg'},
+	{title: '橘猫', url: 'https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg'},
+];
 
-component.mountTo(document.body);
+let list = <ListView data={data}>
+	{record => <figure>
+			<img src={record.url}/>
+			<figcaption>{record.title}</figcaption>
+	</figure>}
+</ListView>
+list.mountTo(document.body);
 
 
