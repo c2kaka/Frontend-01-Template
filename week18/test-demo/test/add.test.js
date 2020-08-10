56044e859dbe29382a33f63d3661dcfde1da0909
Kaka <c2kaka@126.com>
2020-08-10 19:20:02 +0800

    mocha-demo

---
+++ week18/test-demo/test/add.test.js
@@ -0,0 +1,10 @@
+var add = require('../src/add');
+var assert = require('assert');
+
+describe('add', function () {
+  describe('add', function () {
+    it('should return 7 when add(3,4)', function () {
+      assert.equal(add.add(3, 4), 7);
+    });
+  });
+});
