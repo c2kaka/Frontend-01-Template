158042e38e55653aaae6e1d21f5bff68cfae3bed
Kaka <c2kaka@126.com>
2020-08-18 20:26:04 +0800

    'use-archiver'

--- week19/publish/publish-tool/publish.js
+++ week19/publish/publish-tool/publish.js
@@ -1,36 +1,46 @@
 const http = require('http');
 const querystring = require('querystring');
 const fs = require('fs');
+const archiver = require('archiver');
 
-let fileName = './blueCat.jpg';
-fs.stat(fileName, (error, stat) => {
-    console.log(stat);
-    const options = {
-        host: 'localhost',
-        port: 8081,
-        path: '/?filename=blueCat.jpg',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': stat.size
-        }
-    };
+let fileName = './package';
+// fs.stat(fileName, (error, stat) => {
+//     console.log(stat);
+const options = {
+    host: 'localhost',
+    port: 8081,
+    path: '/?filename=package.zip',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        // 'Content-Length': stat.size
+    }
+};
 
-    const req = http.request(options, (res) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    });
-
-    req.on('error', (e) => {
-        console.error(`problem with request: ${e.message}`);
-    });
-
-// Write data to request body
-    let readStream = fs.createReadStream('./blueCat.jpg');
-    readStream.pipe(req);
-    readStream.on('end', () => {
-        req.end();
-    });
-    // req.write(postData);
-    // req.end();
+var archive = archiver('zip', {
+    zlib: {level: 9} // Sets the compression level.
 });
+
+archive.directory(fileName, false);
+
+const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+});
+
+req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+});
+
+archive.pipe(req);
+
+archive.finalize();
+// Write data to request body
+// let readStream = fs.createReadStream('./package');
+// readStream.pipe(req);
+// readStream.on('end', () => {
+//     req.end();
+// });
+// req.write(postData);
+// req.end();
+// });
